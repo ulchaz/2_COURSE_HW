88875bab0bedb1d9e9f5cfa855b7f7b4eba1c012
ulchaz <uliaesaulkova48@gmail.com>
2025-07-25 18:56:53 +0300

    задание 1

---
+++ 2_COURSE_HW/hw3.js
@@ -0,0 +1,7 @@
+let password = 'password';
+let userPassword = prompt('Введите пароль');
+if (userPassword === password) {
+    alert('Пароль введен верно')
+} else {
+    alert('Пароль введен неправильно')
+}
