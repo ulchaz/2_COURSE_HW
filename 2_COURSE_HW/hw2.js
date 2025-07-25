3c81431f0c59d8207dbb972a17fd9382423c0608
ulchaz <uliaesaulkova48@gmail.com>
2025-07-25 18:47:13 +0300

    задание 10

--- 2_COURSE_HW/hw2.js
+++ 2_COURSE_HW/hw2.js
@@ -1,18 +1,2 @@
-let  user = {
-    name: 'Юлия',
-    age: 21,
-    isAdmin: false
-};
-console.log(user)
-
-user.sityOfResidense = 'Москва';
-console.log(user);
-
-user.age = 22;
-console.log(user);
-
-delete user.sityOfResidense
-console.log(user);
-
-let info = prompt('Какую информацию хотите узнать о пользователе?')
-alert(user[info])
+let name = prompt('Ваше имя?');
+alert(`Привет, ${name} !`);
