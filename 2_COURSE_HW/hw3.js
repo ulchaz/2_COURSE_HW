4136ba27a2803900682b438fb74f7bac563402f9
ulchaz <uliaesaulkova48@gmail.com>
2025-07-25 19:03:37 +0300

    задание 3

--- 2_COURSE_HW/hw3.js
+++ 2_COURSE_HW/hw3.js
@@ -1,7 +1,6 @@
-let d = 3;
-let e = 9;
-if (d > 100 || e > 100) {
-    console.log('Верно');
+let c = 4;
+if (c > 0 && c < 100) {
+    console.log('Верно')
 } else {
-    console.log('Неверно');
+    console.log('Неверно')
 }
