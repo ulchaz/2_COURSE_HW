10e8904258f6695d0c5739255bc5545f17e05534
ulchaz <uliaesaulkova48@gmail.com>
2025-07-28 21:54:52 +0300

    задание 4

--- 2_COURSE_HW/hw4.js
+++ 2_COURSE_HW/hw4.js
@@ -1,5 +1,9 @@
-let i = 7;
-while (i < 23) {
-    console.log(i);
-    i++;
+let obj = {
+    "Коля" : '200',
+    "Вася" : '300',
+    "Петя" : '400'
+}
+
+for(let name in obj){
+    alert(`${name} - зарплата ${obj[name]} долларов`)
 }
