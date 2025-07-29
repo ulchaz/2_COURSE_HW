29dbed083f815e6582bf63060ca4d849d2a9ecf9
ulchaz <uliaesaulkova48@gmail.com>
2025-07-29 19:33:52 +0300

    задание 3

--- 2_COURSE_HW/hw6.js
+++ 2_COURSE_HW/hw6.js
@@ -1,8 +1,5 @@
 let numbs = [1, 5, 4, 10, 0, 3];
 
-for (let i = 0; i < numbs.length; i++) {
-    if (numbs[i] == 4) {
-        console.log(i)
-        break
-    }
-}
+numbs = numbs.join(' ');
+
+console.log(numbs);
