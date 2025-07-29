2b3815d83d1964b7b02868535e5f34f453e3db7e
ulchaz <uliaesaulkova48@gmail.com>
2025-07-29 19:53:45 +0300

    задание 4

--- 2_COURSE_HW/hw6.js
+++ 2_COURSE_HW/hw6.js
@@ -1,5 +1,13 @@
-let numbs = [1, 5, 4, 10, 0, 3];
+let array = [];
 
-numbs = numbs.join(' ');
+for (let i = 0; i < 3; i++) {
+    let arr1 = [];
 
-console.log(numbs);
+    for (let j = 0; j < 3; j++) {
+        arr1.push(1); 
+    }
+
+    array.push(arr1);
+}
+
+console.log(array);
