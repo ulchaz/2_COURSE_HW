ddcb27b2fded0a90628fe9c82616614ff52876e8
ulchaz <uliaesaulkova48@gmail.com>
2025-07-29 19:32:04 +0300

    задание 2

--- 2_COURSE_HW/hw6.js
+++ 2_COURSE_HW/hw6.js
@@ -1,6 +1,8 @@
 let numbs = [1, 5, 4, 10, 0, 3];
 
 for (let i = 0; i < numbs.length; i++) {
-    console.log(numbs[i]);
-    if (numbs[i] === 10) break;
+    if (numbs[i] == 4) {
+        console.log(i)
+        break
+    }
 }
