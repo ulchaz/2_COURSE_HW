502d9fad91f8c23b876a0402b9c7dee13b18f075
ulchaz <uliaesaulkova48@gmail.com>
2025-07-29 05:55:22 +0300

    задание 8

--- 2_COURSE_HW/hw5.js
+++ 2_COURSE_HW/hw5.js
@@ -1,25 +1,17 @@
-const circle1 = {
-    radius : 10,
-    getArea(){
-        return (3,14 * this.radius ** 2)
-    },
-    getPerimeter(){
-        return 6,28 * this.radius
+getSeason = (month) => {
+    if (month == 1 || month == 2 || month == 12) {
+        return 'Зима'
+    } else if (month == 3 || month == 4 || month == 5){
+        return 'Весна'
+    } else if (month == 6 || month == 7 || month == 8){
+        return 'Лето'
+    } else if (month == 9 || month == 10 || month == 11){
+        return 'Осень'
+    } else {
+        return 'Ведено некорректное значение'
     }
 }
 
-const circle2 = {
-    radius : 4,
-    getArea(){
-        return (3,14 * this.radius ** 2)
-    },
-    getPerimeter(){
-        return 6,28 * this.radius
-    }
-}
-
-console.log(`Площадь круга 1 равна ${circle1.getArea()}`)
-console.log(`Периметр круга 1 равен ${circle1.getPerimeter()}`)
-
-console.log(`Площадь круга 2 равна ${circle2.getArea()}`)
-console.log(`Периметр круга 2 равен ${circle2.getPerimeter()}`)
+console.log(getSeason(1));
+console.log(getSeason(6));
+console.log(getSeason(19));
