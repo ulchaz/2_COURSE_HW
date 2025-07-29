4aad0ff82ec9f46c6421db5e07a1b5a085db42b6
ulchaz <uliaesaulkova48@gmail.com>
2025-07-29 22:54:48 +0300

    задание 15

--- 2_COURSE_HW/hw6.js
+++ 2_COURSE_HW/hw6.js
@@ -1,12 +1,13 @@
 let arr = [];
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 6; i++) {
     let random = Math.floor(Math.random() * 10);
     arr.push(random);
 }
 console.log(arr);
 
-let evenNum = arr.filter(num => num % 2 == 0);
-
-console.log(evenNum);
+let sum = arr.reduce((acc, value) => 
+    (acc + value));
+let result = sum / arr.length;
+console.log(result);
 
