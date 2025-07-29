0bcecb0001348008963238e59865bcba42989306
ulchaz <uliaesaulkova48@gmail.com>
2025-07-29 04:24:45 +0300

    задание 7

--- 2_COURSE_HW/hw5.js
+++ 2_COURSE_HW/hw5.js
@@ -1,14 +1,25 @@
-let checkNum = () => {
-    let userNum = prompt('Введите число')
-
-    if (isNaN(userNum)) {
-        return 'Переданный параметр не является числом'
-    } else {
-        let cubedUserNum = userNum * userNum * userNum;
-        return `${userNum} в кубе равняется ${cubedUserNum}`
+const circle1 = {
+    radius : 10,
+    getArea(){
+        return (3,14 * this.radius ** 2)
+    },
+    getPerimeter(){
+        return 6,28 * this.radius
     }
 }
 
-let result = checkNum();
+const circle2 = {
+    radius : 4,
+    getArea(){
+        return (3,14 * this.radius ** 2)
+    },
+    getPerimeter(){
+        return 6,28 * this.radius
+    }
+}
 
-console.log(result)
+console.log(`Площадь круга 1 равна ${circle1.getArea()}`)
+console.log(`Периметр круга 1 равен ${circle1.getPerimeter()}`)
+
+console.log(`Площадь круга 2 равна ${circle2.getArea()}`)
+console.log(`Периметр круга 2 равен ${circle2.getPerimeter()}`)
