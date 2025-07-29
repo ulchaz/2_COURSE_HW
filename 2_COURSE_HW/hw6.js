472fa0ee953488704bccd2b85e7b720154d0f30e
ulchaz <uliaesaulkova48@gmail.com>
2025-07-29 19:55:32 +0300

    задание 5

--- 2_COURSE_HW/hw6.js
+++ 2_COURSE_HW/hw6.js
@@ -1,13 +1,5 @@
-let array = [];
+let array = [1, 1, 1];
 
-for (let i = 0; i < 3; i++) {
-    let arr1 = [];
-
-    for (let j = 0; j < 3; j++) {
-        arr1.push(1); 
-    }
-
-    array.push(arr1);
-}
+array.push(2, 2, 2);
 
 console.log(array);
