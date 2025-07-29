31dd90e81b3b0a8746663c0f77b5a70d84db22b9
ulchaz <uliaesaulkova48@gmail.com>
2025-07-29 21:32:19 +0300

    задание 11

--- 2_COURSE_HW/hw6.js
+++ 2_COURSE_HW/hw6.js
@@ -1,7 +1,5 @@
-let num = [1, 3, 5, 7, 9];
+let arr = [1, 3, 5, 7, 9];
+let result = arr.map(num => (num * num));
 
-for (let i = 0; i < num.length - 1; i++){
-    let sum = num[i] + num[i + 1];
-    console.log(sum);   
-}
+console.log(result);
 
