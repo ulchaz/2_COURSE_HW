a05cc5ba4e955b059a6b81624b5fc8d07857cb39
ulchaz <uliaesaulkova48@gmail.com>
2025-08-17 15:28:23 +0300

    задание 8

--- 2_COURSE_HW/hw7.js
+++ 2_COURSE_HW/hw7.js
@@ -1,9 +1,2 @@
-function generateRandomArray (min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-
-    return Math.floor(Math.random() * (max - min)) + min; 
-}
-const result = generateRandomArray(10, 20);
-console.log(result);
-
+let currentDate = new Date();
+console.log(currentDate);
