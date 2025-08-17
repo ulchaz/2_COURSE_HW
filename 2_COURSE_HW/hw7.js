c47ea231a5612853c51dcbba27a453407d2b184a
ulchaz <uliaesaulkova48@gmail.com>
2025-08-17 16:22:54 +0300

    задание 9

--- 2_COURSE_HW/hw7.js
+++ 2_COURSE_HW/hw7.js
@@ -1,2 +1,8 @@
-let currentDate = new Date();
-console.log(currentDate);
+let myDate = new Date();
+myDate = +myDate;
+
+let days73 = 73 * 24 * 60 * 60 * 1000;
+let searchDate = myDate + days73;
+let daysAfter73 = new Date(searchDate);
+
+console.log(daysAfter73);
