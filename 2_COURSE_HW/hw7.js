537e34b68e8c6ec1967c6cda9e385e93a4089287
ulchaz <uliaesaulkova48@gmail.com>
2025-08-17 14:56:19 +0300

    задание 6

--- 2_COURSE_HW/hw7.js
+++ 2_COURSE_HW/hw7.js
@@ -1,6 +1,12 @@
-function getRandomNum(){
-    const randomNum = Math.floor(Math.random() * 10) + 1;
-    console.log(randomNum);
-};
+function generateRandomArray (n) {
+    const length = Math.floor(n / 2);
+    const randomArray = [];
+    for (let i = 0; i < length; i++) {
+        const randomNumber = Math.floor(Math.random() * n);
+        randomArray.push(randomNumber)
+    }  
+    return randomArray;      
+}
+const result = generateRandomArray(16);
+console.log(result);
 
-getRandomNum();
