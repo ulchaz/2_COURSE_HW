2726c45af88742f07b25a0f3d672d9bc26495f11
ulchaz <uliaesaulkova48@gmail.com>
2025-12-27 14:40:20 +0300

    задание 3

--- 2_COURSE_HW/hw8.js
+++ 2_COURSE_HW/hw8.js
@@ -1,27 +1,8 @@
-function isPositive(number) {
-    return number > 0;
-    }
-    function isMale(person) {
-    return person.gender === 'male'
-    }
-    function filter(arr, ruleFunction) {
-    const result = [];
+const intervalId = setInterval(() => {
+    console.log(new Date());
+}, 3000);
 
-    for (let i = 0; i < arr.length; i++){
-        if (ruleFunction(arr[i])) {
-            result.push(arr[i])
-        }
-    }
-    return result
-    }
-    
-    console.log(filter([3, -4, 1, 9], isPositive));
-    
-    const people = [
-       {name: 'Глеб', gender: 'male'},
-       {name: 'Анна', gender: 'female'},
-       {name: 'Олег', gender: 'male'},
-       {name: 'Оксана', gender: 'female'}
-    ];
-    
-    console.log(filter(people, isMale));
+setTimeout(() => {
+    clearInterval(intervalId);
+    console.log('30 секунд прошло')
+}, 30000)
