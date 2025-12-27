ab467a64ca082162193827bab02ff6aa1bf98695
ulchaz <uliaesaulkova48@gmail.com>
2025-12-27 14:35:01 +0300

    задание 2

--- 2_COURSE_HW/hw8.js
+++ 2_COURSE_HW/hw8.js
@@ -1,9 +1,27 @@
-const people = [
-    { name: 'Глеб', age: 29 },
-    { name: 'Анна', age: 17 },
-    { name: 'Олег', age: 7 },
-    { name: 'Оксана', age: 47 }
- ];
- 
- // Допишите колбэк для sort, изучите, как работает колбэк, в документации
- console.log(people.sort((a, b) => a.age - b.age));
+function isPositive(number) {
+    return number > 0;
+    }
+    function isMale(person) {
+    return person.gender === 'male'
+    }
+    function filter(arr, ruleFunction) {
+    const result = [];
+
+    for (let i = 0; i < arr.length; i++){
+        if (ruleFunction(arr[i])) {
+            result.push(arr[i])
+        }
+    }
+    return result
+    }
+    
+    console.log(filter([3, -4, 1, 9], isPositive));
+    
+    const people = [
+       {name: 'Глеб', gender: 'male'},
+       {name: 'Анна', gender: 'female'},
+       {name: 'Олег', gender: 'male'},
+       {name: 'Оксана', gender: 'female'}
+    ];
+    
+    console.log(filter(people, isMale));
