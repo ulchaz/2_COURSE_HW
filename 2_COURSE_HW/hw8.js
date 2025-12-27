61135add2029d95282d8b1e142459fad9e30ca2a
ulchaz <uliaesaulkova48@gmail.com>
2025-12-27 14:45:33 +0300

    задание 5

--- 2_COURSE_HW/hw8.js
+++ 2_COURSE_HW/hw8.js
@@ -1,8 +1,19 @@
-function delayForSecond(callback) {
-    // Код писать можно только внутри этой функции
-   setTimeout(callback, 1000)
+// Функция delayForSecond через 1 секунду пишет в консоль 
+// «Прошла одна секунда», а затем вызывает переданный колбэк
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+        if(cb) {  cb(); }
+    }, 1000)
 }
 
-delayForSecond(function () {
-   console.log('Привет, Глеб!');
-})
+// Функция sayHi выводит в консоль приветствие для указанного имени
+function sayHi (name) {
+    console.log(`Привет, ${name}!`);
+}
+
+// Код выше менять нельзя
+
+// Нужно изменить код ниже:
+delayForSecond(() => sayHi('Глеб'));
+
